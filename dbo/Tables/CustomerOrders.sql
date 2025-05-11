5054df2d771aee63f9e6bee2ae1420014d6389c2
varun kumar <varun kumar@DESKTOP-CQ7II7E>
2025-05-11 00:19:38 -0400

    #4570

--- dbo/Tables/CustomerOrders.sql
+++ dbo/Tables/CustomerOrders.sql
@@ -3,7 +3,7 @@
     [CustomerId]   INT           NULL,
     [OrderDate]    DATE          NULL,
     [TotalAmount]  INT           NULL,
-    [Status]       NVARCHAR (50) NULL,
+    [Status]       NVARCHAR (100) NULL,
     [CreateUser]   NVARCHAR (50) DEFAULT (suser_sname()) NULL,
     [CreatedDate]  DATE          DEFAULT (getdate()) NULL,
     [ModifydUser]  NVARCHAR (50) DEFAULT (suser_sname()) NULL,
